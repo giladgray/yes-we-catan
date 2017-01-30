1692cb39e712578c53c0519268c2857bb23e82ea
Omer Bar-or <omer@bar-or.org>
2017-01-29 16:59:34 -0800

    Adds model, empty view, and initial version of internal graph.

--- server/main.py
+++ server/main.py
@@ -1,18 +1,57 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
+import view
+import webapp2
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
-@app.route('/')
-def hello():
+class Hello(webapp2.RequestHandler):
+  def get(self):
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return_val = """
+{ "tiles": [ [ {"resource": "WHEAT", "number_token": 5},
+           {"resource": "SHEEP", "number_token": 2},
+           {"resource": "DESERT"} ],
+         [ {"resource": "WOOD", "number_token": 8},
+           {"resource": "STONE", "number_token": 10},
+           {"resource": "BRICK", "number_token": 9},
+     {"resource": "WOOD", "number_token": 6} ],
+   [ {"resource": "WHEAT", "number_token": 4},
+     {"resource": "BRICK", "number_token": 3},
+     {"resource": "SHEEP", "number_token": 11},
+     {"resource": "ORE", "number_token": 4},
+     {"resource": "WHEAT", "number_token": 3} ],
+   [ {"resource": "SHEEP", "number_token": 11},
+           {"resource": "ORE", "number_token": 6},
+           {"resource": "BRICK", "number_token": 5},
+           {"resource": "WOOD", "number_token": 8} ],
+         [ {"resource": "WOOD", "number_token": 12},
+           {"resource": "SHEEP", "number_token": 9},
+           {"resource": "WHEAT", "number_token": 10} ] ],
+  "ports": [ "ANY", "ANY", "BRICK", "WOOD", "ANY", "WHEAT", "STONE", "ANY", "SHEEP" ],
+  "id": 1234
+}"""
+    self.response.headers['Content-Type'] = 'application/json'
+    self.response.write(return_val)
 
+class GraphTile(object):
+  neighbor_n = None
+  neighbor_ne = None
+  neighbor_se = None
+  neighbor_s = None
+  neighbor_sw = None
+  neighbor_nw = None
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+  resource = None
+  number_token = None
+  is_port = None
+
+class Generate(webapp2.RequestHandler):
+  def get(self):
+    board = self.GenerateBoard()
+    self.response.headers['Content-Type'] = 'application/json'
+    self.response.write(view.RenderBoard(board))
+
+app = webapp2.WSGIApplication([
+    ('/', Hello),
+    ('/generate', Generate),
+    ], debug=True)
