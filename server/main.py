3a07907c70251477a08fab9bf7dd3a0d3052ad8a
Shir Yehoshua <shiryehoshua@gmail.com>
2017-02-01 21:26:52 -0800

    Add an empty board.

--- server/main.py
+++ server/main.py
@@ -1,39 +1,10 @@
+import json
 import view
 import webapp2
 
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
-
-class Hello(webapp2.RequestHandler):
-
-  def get(self):
-    """Return a friendly HTTP greeting."""
-    return_val = """
-{ "tiles": [ [ {"resource": "WHEAT", "number_token": 5},
-{"resource": "SHEEP", "number_token": 2},
-{"resource": "DESERT"} ],
-[ {"resource": "WOOD", "number_token": 8},
-{"resource": "STONE", "number_token": 10},
-{"resource": "BRICK", "number_token": 9},
-{"resource": "WOOD", "number_token": 6} ],
-[ {"resource": "WHEAT", "number_token": 4},
-{"resource": "BRICK", "number_token": 3},
-{"resource": "SHEEP", "number_token": 11},
-{"resource": "ORE", "number_token": 4},
-{"resource": "WHEAT", "number_token": 3} ],
-[ {"resource": "SHEEP", "number_token": 11},
-{"resource": "ORE", "number_token": 6},
-{"resource": "BRICK", "number_token": 5},
-{"resource": "WOOD", "number_token": 8} ],
-[ {"resource": "WOOD", "number_token": 12},
-{"resource": "SHEEP", "number_token": 9},
-{"resource": "WHEAT", "number_token": 10} ] ],
-"ports": [ "ANY", "ANY", "BRICK", "WOOD", "ANY", "WHEAT", "STONE", "ANY", "SHEEP" ],
-"id": 1234
-}"""
-    self.response.headers['Content-Type'] = 'application/json'
-    self.response.write(return_val)
 
 
 class GraphTile(object):
@@ -49,7 +20,7 @@ class GraphTile(object):
   is_port = None
 
   def GetResourceShort(self):
-    return self.resource[0:2]
+    return self.resource[0:2] if self.resource else "  "
 
   def GetTwoDigitNumber(self):
     return "%s%s" % (self.number_token if self.number_token else " ",
@@ -70,8 +41,115 @@ class GraphTile(object):
            self.neighbor_sw.GetResourceShort() if self.neighbor_sw else "  ",
            self.GetTwoDigitNumber(),
            self.neighbor_se.GetResourceShort() if self.neighbor_se else "  ",
-           self.neighbor_s.GetResourceShort() if self.neighbor_s else "  ",
-           )
+           self.neighbor_s.GetResourceShort() if self.neighbor_s else "  ")
+
+
+class Board:
+  root_tile = None
+  tile_array = None
+
+  def __init__(self):
+    """Creates a skeleton of a board without any resources/ports/etc filled."""
+    self.tile_array = []
+
+    # Create all the tiles.
+    for i in range(5):
+      row = []
+      for j in range(5 - abs(i - 2)):
+        row.append(GraphTile())
+      self.tile_array.append(row)
+
+    # Set north the directions.
+    for row in self.tile_array:
+      prev_tile = None
+      for tile in row:
+        if prev_tile:
+          prev_tile.neighbor_n = tile
+          tile.neighbor_s = prev_tile
+
+  def ToJson(self):
+    tiles = []
+    for row in self.tile_array:
+      new_row = []
+      for tile in row:
+        new_row.append(
+            {"resource": tile.resource,
+             "number_token": tile.number_token if tile.number_token else ""})
+      tiles.append(new_row)
+    return json.dumps({"tiles": tiles})
+
+  def __str__(self):
+    template = """
+                --
+              / %s \ 
+           --        --
+         / %s \ %s / %s \ 
+      --        --        --
+    / %s \ %s / %s \ %s / %s \ 
+           --        --
+    \ %s / %s \ %s / %s \ %s /
+      --        --        --
+    / %s \ %s / %s \ %s / %s \ 
+           --        --
+    \ %s / %s \ %s / %s \ %s /
+      --        --        --
+    / %s \ %s / %s \ %s / %s \ 
+           --        --
+    \ %s / %s \ %s / %s \ %s /
+      --        --        --
+         \ %s / %s \ %s /
+           --        --
+              \ %s /
+                --
+    """
+    return template % (
+        self.tile_array[2][4].GetResourceShort(),
+
+        self.tile_array[1][3].GetResourceShort(),
+        self.tile_array[2][4].GetTwoDigitNumber(),
+        self.tile_array[3][3].GetResourceShort(),
+
+        self.tile_array[0][2].GetResourceShort(),
+        self.tile_array[1][3].GetTwoDigitNumber(),
+        self.tile_array[2][3].GetResourceShort(),
+        self.tile_array[3][3].GetTwoDigitNumber(),
+        self.tile_array[4][2].GetResourceShort(),
+
+        self.tile_array[0][2].GetTwoDigitNumber(),
+        self.tile_array[1][2].GetResourceShort(),
+        self.tile_array[2][3].GetTwoDigitNumber(),
+        self.tile_array[3][2].GetResourceShort(),
+        self.tile_array[4][2].GetTwoDigitNumber(),
+
+        self.tile_array[0][1].GetResourceShort(),
+        self.tile_array[1][2].GetTwoDigitNumber(),
+        self.tile_array[2][2].GetResourceShort(),
+        self.tile_array[3][2].GetTwoDigitNumber(),
+        self.tile_array[4][1].GetResourceShort(),
+
+        self.tile_array[0][1].GetTwoDigitNumber(),
+        self.tile_array[1][1].GetResourceShort(),
+        self.tile_array[2][2].GetTwoDigitNumber(),
+        self.tile_array[3][1].GetResourceShort(),
+        self.tile_array[4][1].GetTwoDigitNumber(),
+
+        self.tile_array[0][0].GetResourceShort(),
+        self.tile_array[1][1].GetTwoDigitNumber(),
+        self.tile_array[2][1].GetResourceShort(),
+        self.tile_array[3][1].GetTwoDigitNumber(),
+        self.tile_array[4][0].GetResourceShort(),
+
+        self.tile_array[0][0].GetTwoDigitNumber(),
+        self.tile_array[1][0].GetResourceShort(),
+        self.tile_array[2][1].GetTwoDigitNumber(),
+        self.tile_array[3][0].GetResourceShort(),
+        self.tile_array[4][0].GetTwoDigitNumber(),
+
+        self.tile_array[1][0].GetTwoDigitNumber(),
+        self.tile_array[2][0].GetResourceShort(),
+        self.tile_array[3][0].GetTwoDigitNumber(),
+
+        self.tile_array[2][0].GetTwoDigitNumber())
 
 
 def GenerateBoard():
@@ -139,21 +217,19 @@ def GenerateBoard():
 class GenerateJson(webapp2.RequestHandler):
 
   def get(self):
-    board = GenerateBoard()
+    board = Board()
     self.response.headers['Content-Type'] = 'application/json'
-    self.response.write(view.RenderJsonBoard(board))
+    self.response.write(board.ToJson())
 
 
 class GenerateString(webapp2.RequestHandler):
 
   def get(self):
-    board = GenerateBoard()
-    s = view.RenderStringBoard(board)
-    print s
-    self.response.write(s)
+    board = Board()
+    print board
+    self.response.write("GAH")
 
 app = webapp2.WSGIApplication([
-    ('/', Hello),
     ('/generate_json', GenerateJson),
     ('/generate_string', GenerateString)
 ], debug=True)
