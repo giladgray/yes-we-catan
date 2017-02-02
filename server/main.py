e167662ea17b2e88ce9fdfdabea7903d25afdc9b
Shir Yehoshua <shiryehoshua@gmail.com>
2017-02-01 20:43:19 -0800

    Add generate_string endpoint for debugging.

--- server/main.py
+++ server/main.py
@@ -51,6 +51,10 @@ class GraphTile(object):
   def GetResourceShort(self):
     return self.resource[0:2]
 
+  def GetTwoDigitNumber(self):
+    return "%s%s" % (self.number_token if self.number_token else " ",
+                     " " if self.number_token < 10 else "")
+
   def __str__(self):
     return """
          %s
@@ -59,87 +63,97 @@ class GraphTile(object):
     %s \ %s / %s
          --
          %s
-    """ % (self.neighbor_n.GetResourceShort() if self.neighbor_n else "XX",
-           self.neighbor_nw.GetResourceShort() if self.neighbor_nw else "XX",
+    """ % (self.neighbor_n.GetResourceShort() if self.neighbor_n else "  ",
+           self.neighbor_nw.GetResourceShort() if self.neighbor_nw else "  ",
            self.GetResourceShort(),
-           self.neighbor_ne.GetResourceShort() if self.neighbor_ne else "XX",
-           self.neighbor_sw.GetResourceShort() if self.neighbor_sw else "XX",
-           "%s%s" % (self.number_token, " " if self.number_token < 10 else ""),
-           self.neighbor_se.GetResourceShort() if self.neighbor_se else "XX",
-           self.neighbor_s.GetResourceShort() if self.neighbor_s else "XX",
+           self.neighbor_ne.GetResourceShort() if self.neighbor_ne else "  ",
+           self.neighbor_sw.GetResourceShort() if self.neighbor_sw else "  ",
+           self.GetTwoDigitNumber(),
+           self.neighbor_se.GetResourceShort() if self.neighbor_se else "  ",
+           self.neighbor_s.GetResourceShort() if self.neighbor_s else "  ",
            )
 
 
-class Generate(webapp2.RequestHandler):
+def GenerateBoard():
+  json_board = {"tiles": [[{"resource": "WHEAT", "number_token": 5},
+                           {"resource": "SHEEP", "number_token": 2},
+                           {"resource": "DESERT"}],
+                          [{"resource": "WOOD", "number_token": 8},
+                           {"resource": "STONE", "number_token": 10},
+                           {"resource": "BRICK", "number_token": 9},
+                           {"resource": "WOOD", "number_token": 6}],
+                          [{"resource": "WHEAT", "number_token": 4},
+                           {"resource": "BRICK", "number_token": 3},
+                           {"resource": "SHEEP", "number_token": 11},
+                           {"resource": "ORE", "number_token": 4},
+                           {"resource": "WHEAT", "number_token": 3}],
+                          [{"resource": "SHEEP", "number_token": 11},
+                           {"resource": "ORE", "number_token": 6},
+                           {"resource": "BRICK", "number_token": 5},
+                           {"resource": "WOOD", "number_token": 8}],
+                          [{"resource": "WOOD", "number_token": 12},
+                           {"resource": "SHEEP", "number_token": 9},
+                           {"resource": "WHEAT", "number_token": 10}]],
+                "ports": ["ANY", "ANY", "BRICK", "WOOD", "ANY", "WHEAT", "STONE", "ANY", "SHEEP"],
+                "id": 1234}
+  graph_board = []
+  for tile_row in json_board["tiles"]:
+    graph_row = []
+    for tile in tile_row:
+      graph_tile = GraphTile()
+      graph_tile.resource = tile["resource"]
+      graph_tile.number_token = tile[
+          "number_token"] if "number_token" in tile else None
+      graph_row.append(graph_tile)
+    graph_board.append(graph_row)
 
-  def GenerateBoard(board):
-    json_board = {"tiles": [[{"resource": "WHEAT", "number_token": 5},
-                             {"resource": "SHEEP", "number_token": 2},
-                             {"resource": "DESERT"}],
-                            [{"resource": "WOOD", "number_token": 8},
-                             {"resource": "STONE", "number_token": 10},
-                             {"resource": "BRICK", "number_token": 9},
-                             {"resource": "WOOD", "number_token": 6}],
-                            [{"resource": "WHEAT", "number_token": 4},
-                             {"resource": "BRICK", "number_token": 3},
-                             {"resource": "SHEEP", "number_token": 11},
-                             {"resource": "ORE", "number_token": 4},
-                             {"resource": "WHEAT", "number_token": 3}],
-                            [{"resource": "SHEEP", "number_token": 11},
-                             {"resource": "ORE", "number_token": 6},
-                             {"resource": "BRICK", "number_token": 5},
-                             {"resource": "WOOD", "number_token": 8}],
-                            [{"resource": "WOOD", "number_token": 12},
-                             {"resource": "SHEEP", "number_token": 9},
-                             {"resource": "WHEAT", "number_token": 10}]],
-                  "ports": ["ANY", "ANY", "BRICK", "WOOD", "ANY", "WHEAT", "STONE", "ANY", "SHEEP"],
-                  "id": 1234}
-    graph_board = []
-    for tile_row in json_board["tiles"]:
-      graph_row = []
-      for tile in tile_row:
-        graph_tile = GraphTile()
-        graph_tile.resource = tile["resource"]
-        graph_tile.number_token = tile[
-            "number_token"] if "number_token" in tile else None
-        graph_row.append(graph_tile)
-      graph_board.append(graph_row)
+  row = 0
+  col = 0
+  # TODO do not hard code
+  for row in range(len(graph_board)):
+    # This denotes the shift of the adjacent row's column index based on
+    # direction and location within the board (assuming a 5 row board).
+    ne_shift = 1 if row < 2 else 0
+    se_shift = 0 if row < 2 else -1
+    nw_shift = 0 if row < 3 else 1
+    sw_shift = -1 if row < 3 else 0
+    for col in range(len(graph_board[row])):
+      def GetTileOrNone(graph_board, row, col):
+        if (row >= 0 and row < len(graph_board) and
+                col >= 0 and col < len(graph_board[row])):
+          return graph_board[row][col]
+        else:
+          return None
 
-    row = 0
-    col = 0
-    # TODO do not hard code
-    for row in range(len(graph_board)):
-      # This denotes the shift of the adjacent row's column index based on
-      # direction and location within the board (assuming a 5 row board).
-      ne_shift = 1 if row < 2 else 0
-      se_shift = 0 if row < 2 else -1
-      nw_shift = 0 if row < 3 else 1
-      sw_shift = -1 if row < 3 else 0
-      for col in range(len(graph_board[row])):
-        def GetTileOrNone(graph_board, row, col):
-          if (row >= 0 and row < len(graph_board) and
-                  col >= 0 and col < len(graph_board[row])):
-            return graph_board[row][col]
-          else:
-            return None
+      curr = graph_board[row][col]
+      curr.neighbor_n = GetTileOrNone(graph_board, row, col + 1)
+      curr.neighbor_ne = GetTileOrNone(graph_board, row + 1, col + ne_shift)
+      curr.neighbor_se = GetTileOrNone(graph_board, row + 1, col + se_shift)
+      curr.neighbor_s = GetTileOrNone(graph_board, row, col - 1)
+      curr.neighbor_nw = GetTileOrNone(graph_board, row - 1, col + nw_shift)
+      curr.neighbor_sw = GetTileOrNone(graph_board, row - 1, col + sw_shift)
 
-        curr = graph_board[row][col]
-        curr.neighbor_n = GetTileOrNone(graph_board, row, col + 1)
-        curr.neighbor_ne = GetTileOrNone(graph_board, row + 1, col + ne_shift)
-        curr.neighbor_se = GetTileOrNone(graph_board, row + 1, col + se_shift)
-        curr.neighbor_s = GetTileOrNone(graph_board, row, col - 1)
-        curr.neighbor_nw = GetTileOrNone(graph_board, row - 1, col + nw_shift)
-        curr.neighbor_sw = GetTileOrNone(graph_board, row - 1, col + sw_shift)
-        print "For row=%s,col=%s: %s" % (row, col, curr)
+  return graph_board[0][0]
 
-    return graph_board[0][0]
+
+class GenerateJson(webapp2.RequestHandler):
 
   def get(self):
-    board = self.GenerateBoard()
+    board = GenerateBoard()
     self.response.headers['Content-Type'] = 'application/json'
-    self.response.write(view.RenderBoard(board))
+    self.response.write(view.RenderJsonBoard(board))
+
+
+class GenerateString(webapp2.RequestHandler):
+
+  def get(self):
+    board = GenerateBoard()
+    s = view.RenderStringBoard(board)
+    print s
+    self.response.write(s)
 
 app = webapp2.WSGIApplication([
     ('/', Hello),
-    ('/generate', Generate),
+    ('/generate_json', GenerateJson),
+    ('/generate_string', GenerateString)
 ], debug=True)
--- server/view.py
+++ server/view.py
@@ -1,22 +1,27 @@
 import json
 
 
-def RenderJsonBoard(root_tile):
+def _GetTileMatrix(root_tile):
   ordered_tiles = []
   # First grab the tile for the beginning of each row.
   curr = root_tile
   while (curr != None):
     ordered_tiles.append([curr])
     # We always go east, but choose the non-null one.
-    curr = curr.neighbor_ne if curr.neighbor_ne != None else curr.neighbor_se
+    curr = curr.neighbor_se if curr.neighbor_se != None else curr.neighbor_ne
 
+  i = 0
   for row in ordered_tiles:
+    i += 1
     curr = row[0].neighbor_n
     while (curr != None):
       row.append(curr)
       curr = curr.neighbor_n
+  return ordered_tiles
 
-  print "num_ordered_tiles: %s" % len(ordered_tiles)
+
+def RenderJsonBoard(root_tile):
+  ordered_tiles = _GetTileMatrix(root_tile)
 
   tiles = []
   for row in ordered_tiles:
@@ -27,5 +32,83 @@ def RenderJsonBoard(root_tile):
            "number_token": tile.number_token if tile.number_token else ""})
     tiles.append(new_row)
 
-  return json.dumps({
-      "tiles": tiles})
+  return json.dumps({"tiles": tiles})
+
+
+def RenderStringBoard(root_tile):
+  template = """
+_                --<br>
+_              / %s \<br>
+_           --        --<br>
+_         / %s \ %s / %s \<br>
+_      --        --        --<br>
+_    / %s \ %s / %s \ %s / %s \<br>
+_           --        --<br>
+_    \ %s / %s \ %s / %s \ %s /<br>
+_      --        --        --<br>
+_    / %s \ %s / %s \ %s / %s \<br>
+_           --        --<br>
+_    \ %s / %s \ %s / %s \ %s /<br>
+_      --        --        --<br>
+_    / %s \ %s / %s \ %s / %s \<br>
+_           --        --<br>
+_    \ %s / %s \ %s / %s \ %s /<br>
+_      --        --        --<br>
+_         \ %s / %s \ %s /<br>
+_           --        --<br>
+_              \ %s /<br>
+_                --<br>
+  """
+  ordered_tiles = _GetTileMatrix(root_tile)
+  s = ""
+  for row in ordered_tiles:
+    print["%s-%s" % (tile.GetResourceShort(), tile.GetTwoDigitNumber()) for tile in row]
+
+  return template % (
+      ordered_tiles[2][4].GetResourceShort(),
+
+      ordered_tiles[1][3].GetResourceShort(),
+      ordered_tiles[2][4].GetTwoDigitNumber(),
+      ordered_tiles[3][3].GetResourceShort(),
+
+      ordered_tiles[0][2].GetResourceShort(),
+      ordered_tiles[1][3].GetTwoDigitNumber(),
+      ordered_tiles[2][3].GetResourceShort(),
+      ordered_tiles[3][3].GetTwoDigitNumber(),
+      ordered_tiles[4][2].GetResourceShort(),
+
+      ordered_tiles[0][2].GetTwoDigitNumber(),
+      ordered_tiles[1][2].GetResourceShort(),
+      ordered_tiles[2][3].GetTwoDigitNumber(),
+      ordered_tiles[3][2].GetResourceShort(),
+      ordered_tiles[4][2].GetTwoDigitNumber(),
+
+      ordered_tiles[0][1].GetResourceShort(),
+      ordered_tiles[1][2].GetTwoDigitNumber(),
+      ordered_tiles[2][2].GetResourceShort(),
+      ordered_tiles[3][2].GetTwoDigitNumber(),
+      ordered_tiles[4][1].GetResourceShort(),
+
+      ordered_tiles[0][1].GetTwoDigitNumber(),
+      ordered_tiles[1][1].GetResourceShort(),
+      ordered_tiles[2][2].GetTwoDigitNumber(),
+      ordered_tiles[3][1].GetResourceShort(),
+      ordered_tiles[4][1].GetTwoDigitNumber(),
+
+      ordered_tiles[0][0].GetResourceShort(),
+      ordered_tiles[1][1].GetTwoDigitNumber(),
+      ordered_tiles[2][1].GetResourceShort(),
+      ordered_tiles[3][1].GetTwoDigitNumber(),
+      ordered_tiles[4][0].GetResourceShort(),
+
+      ordered_tiles[0][0].GetTwoDigitNumber(),
+      ordered_tiles[1][0].GetResourceShort(),
+      ordered_tiles[2][1].GetTwoDigitNumber(),
+      ordered_tiles[3][0].GetResourceShort(),
+      ordered_tiles[4][0].GetTwoDigitNumber(),
+
+      ordered_tiles[1][0].GetTwoDigitNumber(),
+      ordered_tiles[2][0].GetResourceShort(),
+      ordered_tiles[3][0].GetTwoDigitNumber(),
+
+      ordered_tiles[2][0].GetTwoDigitNumber())
